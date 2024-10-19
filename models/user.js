ca5f96c5c0c32a9ae5a694044b1201f435acf4c2
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-19 23:22:47 +0530

    first commit

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const passwordComplexity = require("joi-password-complexity");
 const userSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
-	email: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 });
 
