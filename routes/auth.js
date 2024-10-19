40f345092217c96dd0ed6fdabf98b562d4f6e29f
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-19 22:27:01 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User } = require("../models/user"); // Ensure this path is correct
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 
 // Login route
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, validate } = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // Create user
 router.post("/", async (req, res) => {
